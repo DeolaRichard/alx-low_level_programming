62c730a7b8d44bea5c4b5a76f08f0be86c7eef73
DeolaRichard <adekunlerichie5@gmail.com>
2022-12-13 09:10:05 -0800

    print numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,12 +6,11 @@
  */
 
 int main(void)
-
 {
 	int num;
 
 	for (num = 0; num < 10; num++)
-	putchar((num % 10), +'0');
-	putchar('\n');
+		putchar((num % 10), +'0');
+	putchar("\n");
 	return (0);
 }
