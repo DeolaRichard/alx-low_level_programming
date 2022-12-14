3a8d443d03b4781b23f2e550ab7dc879fac4bfca
DeolaRichard <adekunlerichie5@gmail.com>
2022-12-14 00:50:15 -0800

    print characters and a new line

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * _putchar - writes the charcater to stdout
+ * @c: the character to print
+ *
+ * Return: On success 1
+ */
+int _putchar(char c)
+	return (write(1, &c, 1));
